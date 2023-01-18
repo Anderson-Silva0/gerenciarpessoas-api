73ca20445a3f2f7eea16d13a70a506613dfa8cdd
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-18 08:54:07 -0300

    RegraDeNegocioException adicionada

---
+++ src/main/java/com/attornatus/gerenciarpessoas/exception/RegraDeNegocioException.java
@@ -0,0 +1,7 @@
+package com.attornatus.gerenciarpessoas.exception;
+
+public class RegraDeNegocioException extends RuntimeException {
+    public RegraDeNegocioException(String msg) {
+        super(msg);
+    }
+}

97c34f919ed0243194703c7b2c5db9331c346cfd
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-18 08:10:07 -0300

    EnderecoRepository adicionado

---
+++ src/main/java/com/attornatus/gerenciarpessoas/repository/EnderecoRepository.java
@@ -0,0 +1,8 @@
+package com.attornatus.gerenciarpessoas.model.repository;
+
+import com.attornatus.gerenciarpessoas.model.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+}

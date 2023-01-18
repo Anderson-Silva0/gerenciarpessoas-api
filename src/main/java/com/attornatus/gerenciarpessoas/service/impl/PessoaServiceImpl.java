aaeb2e8f9fbbaf262e0f2dc9552122a6715675cb
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-18 17:02:59 -0300

    PessoaServiceImpl finalizado

--- src/main/java/com/attornatus/gerenciarpessoas/service/impl/PessoaServiceImpl.java
+++ src/main/java/com/attornatus/gerenciarpessoas/service/impl/PessoaServiceImpl.java
@@ -5,6 +5,7 @@ import com.attornatus.gerenciarpessoas.model.entity.Pessoa;
 import com.attornatus.gerenciarpessoas.model.repository.PessoaRepository;
 import com.attornatus.gerenciarpessoas.service.PessoaService;
 import com.attornatus.gerenciarpessoas.service.exception.DataIntegratyViolationException;
+import com.attornatus.gerenciarpessoas.service.exception.ObjectNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,20 +24,26 @@ public class PessoaServiceImpl implements PessoaService {
 
     @Override
     public Pessoa criarPessoa(PessoaDTO pessoaDTO) {
-        return repository.save( mapper.map(pessoaDTO, Pessoa.class) );
+            if(pessoaDTO.getId() != null && pessoaDTO.getId().equals("")) {
+                throw new DataIntegratyViolationException("Para salvar um usuário nao precisa informar id.");
+            }
+            return repository.save( mapper.map(pessoaDTO, Pessoa.class) );
     }
 
     @Override
     public Pessoa editarPessoa(PessoaDTO pessoaDTO) {
-        if (pessoaDTO.getId() != null)
-            return repository.save( mapper.map(pessoaDTO, Pessoa.class) );
-        else
-            throw new DataIntegratyViolationException("Erro ao tentar atualizar pessoa, informe o Id.");
+        if(!repository.existsById(pessoaDTO.getId())){
+            throw new ObjectNotFoundException("Pessoa não encontrada para o Id informado.");
+        }
+        return repository.save( mapper.map(pessoaDTO, Pessoa.class) );
     }
 
     @Override
-    public Optional<Pessoa> consultarPessoa(PessoaDTO pessoaDTO) {
-        return repository.findById(pessoaDTO.getId());
+    public Optional<Pessoa> consultarPessoa(Long id ) {
+        if(repository.existsById(id))
+            return repository.findById(id);
+        else
+            throw new ObjectNotFoundException("Impossível consultar Pessoa. Informe um Id válido.");
     }
 
     @Override

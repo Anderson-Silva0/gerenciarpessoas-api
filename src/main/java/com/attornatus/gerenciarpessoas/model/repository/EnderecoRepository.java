2528e832a3e4fb22dd7385cf0946384742f7b85f
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-18 17:05:52 -0300

    modificacao no repository

--- src/main/java/com/attornatus/gerenciarpessoas/model/repository/EnderecoRepository.java
+++ src/main/java/com/attornatus/gerenciarpessoas/model/repository/EnderecoRepository.java
@@ -2,6 +2,9 @@ package com.attornatus.gerenciarpessoas.model.repository;
 
 import com.attornatus.gerenciarpessoas.model.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+    //List<Endereco> findAllByIdPessoa();
 }
--- src/main/java/com/attornatus/gerenciarpessoas/model/repository/PessoaRepository.java
+++ src/main/java/com/attornatus/gerenciarpessoas/model/repository/PessoaRepository.java
@@ -2,7 +2,9 @@ package com.attornatus.gerenciarpessoas.model.repository;
 
 import com.attornatus.gerenciarpessoas.model.entity.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 
 }

98dda8b7d5c19240ba42b3c0cd1039c759af05da
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-18 12:18:46 -0300

    PessoaController adicionada

--- src/main/java/com/attornatus/gerenciarpessoas/api/controller/PessoaController.java
+++ src/main/java/com/attornatus/gerenciarpessoas/api/controller/PessoaController.java
@@ -1,5 +1,8 @@
 package com.attornatus.gerenciarpessoas.api.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PessoaController {
     
 }

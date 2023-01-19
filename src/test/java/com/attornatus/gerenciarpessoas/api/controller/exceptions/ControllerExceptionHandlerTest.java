29a03c74a0c2dfc976b8bc9fb7882dc61e9cee5e
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-19 17:43:38 -0300

    ControllerExceptionHandlerTest finalizado

--- src/test/java/com/attornatus/gerenciarpessoas/api/controller/exceptions/ControllerExceptionHandlerTest.java
+++ src/test/java/com/attornatus/gerenciarpessoas/api/controller/exceptions/ControllerExceptionHandlerTest.java
@@ -1,10 +1,17 @@
 package com.attornatus.gerenciarpessoas.api.controller.exceptions;
 
+import com.attornatus.gerenciarpessoas.service.exception.DataIntegratyViolationException;
+import com.attornatus.gerenciarpessoas.service.exception.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,12 +29,35 @@ class ControllerExceptionHandlerTest {
     }
 
     @Test
-    void objectNotFound() {
+    void quandoOcorrerUmaExceptionObjectNotFoundEntaoRetorneUmaResponseEntity() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .objectNotFound(
+                        new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO),
+                        new MockHttpServletRequest());
 
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(OBJETO_NAO_ENCONTRADO, response.getBody().getError());
+        assertEquals(HttpStatus.NOT_FOUND.value(), response.getBody().getStatus());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
     }
 
     @Test
     void dataIntegratyViolationException() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .dataIntegratyViolationException(
+                        new DataIntegratyViolationException(E_MAIL_JA_CADASTRADO),
+                        new MockHttpServletRequest());
 
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(E_MAIL_JA_CADASTRADO, response.getBody().getError());
+        assertEquals(HttpStatus.BAD_REQUEST.value(), response.getBody().getStatus());
     }
 }

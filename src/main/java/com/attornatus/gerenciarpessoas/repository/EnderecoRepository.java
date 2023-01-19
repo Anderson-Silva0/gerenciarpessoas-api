686936a9928b16f873862af68c098e06fa679d50
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-19 12:47:30 -0300

    mudança de pastas

--- src/main/java/com/attornatus/gerenciarpessoas/repository/EnderecoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.attornatus.gerenciarpessoas.model.repository;
-
-import com.attornatus.gerenciarpessoas.model.entity.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-
-}

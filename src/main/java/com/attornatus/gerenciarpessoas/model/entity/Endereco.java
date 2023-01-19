54914aefc86bb1746e0402a2080dcea6b10b356c
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-19 12:41:16 -0300

    alteracao em Endereco

--- src/main/java/com/attornatus/gerenciarpessoas/model/entity/Endereco.java
+++ src/main/java/com/attornatus/gerenciarpessoas/model/entity/Endereco.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "endereco", schema = "gerencia")
@@ -23,32 +23,28 @@ public class Endereco {
     private Long id;
 
     @Column(name = "logradouro")
-    @NotEmpty(message = "O campo Logradouro é obrigátorio.")
+    @NotBlank(message = "O campo Logradouro é obrigátorio.")
     @Length(min = 2, max = 150)
     private String logradouro;
 
     @Column(name = "cep")
-    @NotEmpty(message = "O campo CEP é obrigátorio.")
+    @NotBlank(message = "O campo CEP é obrigátorio.")
     @Length(min = 8, max = 8)
     private String cep;
 
     @Column(name = "numero")
-    @NotEmpty(message = "O campo Numero é obrigátorio.")
     private Long numero;
 
     @Column(name = "cidade")
-    @NotEmpty(message = "O campo Cidade é obrigátorio.")
+    @NotBlank(message = "O campo Cidade é obrigátorio.")
     @Length(min = 2, max = 50)
     private String cidade;
 
     @ManyToOne
-    @JoinColumn(name = "id_pessoa")
-    @NotEmpty(message = "O campo id_pessoa é obrigátorio.")
+    @JoinColumn(name = "pessoa")
     private Pessoa pessoa;
 
-    @Column(name = "endereco_principal")
+    @Column(name = "is_Principal")
     @Enumerated(EnumType.STRING)
-    @NotEmpty(message = "O campo Principal é obrigátorio.")
-    @Length(min = 3, max = 3)
-    private EnderecoPrincipal principal;
+    private EnderecoPrincipal isPrincipal;
 }

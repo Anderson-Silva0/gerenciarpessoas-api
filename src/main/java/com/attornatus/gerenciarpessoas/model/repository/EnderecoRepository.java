77520e9872c03692e51ef39245146b04f7b6795a
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-01-19 12:42:52 -0300

    adicionando query para consulta no banco

--- src/main/java/com/attornatus/gerenciarpessoas/model/repository/EnderecoRepository.java
+++ src/main/java/com/attornatus/gerenciarpessoas/model/repository/EnderecoRepository.java
@@ -2,9 +2,16 @@ package com.attornatus.gerenciarpessoas.model.repository;
 
 import com.attornatus.gerenciarpessoas.model.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-    //List<Endereco> findAllByIdPessoa();
+
+    @Query(value = "select e from Endereco e " +
+            "JOIN  Pessoa p on e.pessoa.id = p.id  and p.id = :id")
+    List<Endereco> buscarTodosEnderecosPorIdPessoa(@Param("id") Long id);
 }
